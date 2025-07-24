e8483135484b206669596f94731027a16fb9cbde
Adriano <abarbosa@img.com.br>
2025-07-24 11:20:02 -0300

    Adicionado api-gateway como interface unica de entrada

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -6,11 +6,14 @@ app.use(express.json());
 
 app.post('/pedido', async (req, res) => {
   try {
-    await axios.post('http://pedido-service:4000/pedido', req.body);
-    res.status(201).send({ mensagem: 'Pedido enviado com sucesso' });
-  } catch (err) {
-    res.status(500).send({ erro: 'Erro ao encaminhar pedido' });
+    const resposta = await axios.post('http://pedido-service:4000/pedido', req.body);
+    res.status(201).send(resposta.data);
+  } catch (erro) {
+    console.error('[✖] Erro ao redirecionar pedido:', erro.message);
+    res.status(500).send({ erro: 'Erro ao encaminhar pedido para o serviço de pedidos' });
   }
 });
 
-app.listen(3000, () => console.log('API Gateway rodando na porta 3000'));
+app.listen(3000, () => {
+  console.log('[🚪] API Gateway rodando na porta 3000');
+});
